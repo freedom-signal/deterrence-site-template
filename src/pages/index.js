56ac84edaa9854164f2e52f6f69a905de4e7e2b6
Kate Kourbatova <ekateko@uw.edu>
2018-03-29 16:36:38 -0700

    Add third page, update nav on first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ul>
+      <li><Link to="/page-2/">Go to page 2</Link></li>
+      <li><Link to="/page-3/">Go to page 3</Link></li>
+    </ul>
   </div>
 )
 
