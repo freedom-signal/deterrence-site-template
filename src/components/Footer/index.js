6292ddeacdcc205b983547011cf3f630f58ff3cf
liz rush <liz@lizmrush.com>
2018-03-30 16:30:59 -0700

    Add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer style={{
+    background: 'purple',
+    color: 'white',
+    padding: '1.45rem 1.0875rem',
+  }}>
+
+    <span>Hello this is the footer!</span>
+  </footer>
+)
+
+export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import Footer from '../components/Footer'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -25,6 +25,8 @@ const TemplateWrapper = ({ children }) => (
     >
       {children()}
     </div>
+
+    <Footer />
   </div>
 )
 
