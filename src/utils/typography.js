1b71d631239d89dd883cbefe340fce20de465528
liz rush <liz@lizmrush.com>
2018-04-20 11:13:14 -0700

    Update fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,12 @@
 import Typography from "typography";
 import bootstrapTheme from "typography-theme-bootstrap";
 
-const typography = new Typography(bootstrapTheme);
-
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.45,
+    headerFontFamily: ['Cera-Bold', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+    bodyFontFamily: ['Cera-Light', 'serif'],
+    // See below for the full list of options.
+  })
+  
 export default typography;
