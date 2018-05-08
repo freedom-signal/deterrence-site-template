c2c867c43882142361a753964d82a18e2bb8b44f
liz rush <liz@lizmrush.com>
2018-05-08 14:35:46 -0700

    remove conflict

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,14 +5,11 @@ const typography = new Typography({
     baseLineHeight: 1.45,
     headerFontFamily: ['Cera-Bold', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
     bodyFontFamily: ['Cera-Light', 'serif'],
-<<<<<<< HEAD
-=======
     overrideStyles: () => ({
       html: {
         overflowY: 'initial',
       },
     })
->>>>>>> Remove overflow-y: scroll on html tag
   });
   
 export default typography;
