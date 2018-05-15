6960c6770ea108455c79a3114a1e2efbfeb8e6f7
liz rush <liz@lizmrush.com>
2018-05-15 14:01:03 -0700

    Add hero image

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Img from 'gatsby-image';
+
 import styles from './contact.module.scss';
 import sharedStyles from '../assets/shared-styles/shared.module.scss';
 
-const HarmPage = () => (
-  <div className={styles.pageWrapper}>
-    <div>
-      <h1>Want to talk?</h1>
-      <div className={sharedStyles.dividerLine}></div>
-      <p>Get help from men who understand this experience.</p>
-      <p>Our resources counselor will connect you with services in your area.</p>
-    </div>
+const ContactPage = ({ data }) => (
+  <div>
+    <Img
+    alt="Text overlay that reads: You are not alone. There is Help available. Background image of man sitting on a wall looking sad and thoughtful."
+    sizes={data.heroImage.sizes}
+    />
 
-    <form>
-      <input className={styles.emailField}
-            type='email' 
-            placeholder='your email address' >
-      </input>
-      <button>Get confidential help</button>
-    </form>
+    <div className={styles.pageWrapper}>
+      <div>
+        <h1>Want to talk?</h1>
+        <div className={sharedStyles.dividerLine}></div>
+        <p>Get help from men who understand this experience.</p>
+        <p>Our resources counselor will connect you with services in your area.</p>
+      </div>
+
+      <form>
+        <input className={styles.emailField}
+              type='email' 
+              placeholder='your email address' >
+        </input>
+        <button>Get confidential help</button>
+      </form>
+    </div>
   </div>
 )
 
-export default HarmPage
+export const query = graphql`
+  query ContactQuery {
+    heroImage: imageSharp(id: {regex: "/contact1/"}) {
+      sizes(maxWidth: 1240) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`
+export default ContactPage
