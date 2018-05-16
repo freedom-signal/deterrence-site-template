fc335362179cebc8e606ac5f6be1b049159e9f10
Kate Kourbatova <ekateko@uw.edu>
2018-05-16 14:57:28 -0700

    Create DesktopMenu component and the header render it for screen sizes wider than 650px

---
+++ src/components/DesktopMenu/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import styles from './index.module.scss';
+
+
+const MenuLink = props => (
+  <div className={styles.menuLink}>
+    <Link to={props.url}>
+      { props.label }
+    </Link>
+  </div>
+)
+
+
+const DesktopMenu = () => (
+  <div>
+    <MenuLink label='Buyer Beware' url='/' aria-label='Home' className={styles.siteTitle}></MenuLink>
+    <div className={styles.rightMenu}>
+      <MenuLink label='Harm' url='/harm'></MenuLink>
+      <MenuLink label='Consequences' url='/consequences'></MenuLink>
+      <MenuLink label='Alternatives' url='/alternatives'></MenuLink>
+      <MenuLink label='Contact' url='/contact'></MenuLink>
+    </div>
+  </div>
+)
+
+
+export default DesktopMenu
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import MediaQuery from 'react-responsive';
 import { slide as Menu } from 'react-burger-menu';
 
 import styles from './index.module.scss';
+import DesktopMenu from '../DesktopMenu/index.js'
 
-const MenuLink = props => (
-  <div className={styles.menuLink}>
-    <Link to={props.url}>
-      { props.label }
-    </Link>
-  </div>
-)
+
 
 const Header = () => (
   <div className={styles.header}>
@@ -21,14 +15,7 @@ const Header = () => (
       </MediaQuery>
    
       <MediaQuery query="(min-width: 651px)">
-        <div>hello</div>
-        {/* <MenuLink label='Buyer Beware' url='/' aria-label='Home' className={styles.siteTitle}></MenuLink>
-        <div className={styles.rightMenu}>
-          <MenuLink label='Harm' url='/harm'></MenuLink>
-          <MenuLink label='Consequences' url='/consequences'></MenuLink>
-          <MenuLink label='Alternatives' url='/alternatives'></MenuLink>
-          <MenuLink label='Contact' url='/contact'></MenuLink>
-        </div> */}
+        <DesktopMenu> </DesktopMenu>
       </MediaQuery>
     </nav>
   </div>
