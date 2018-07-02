a2b8074a2d2e26f78f1697556156c84a0e525954
Kate Kourbatova <ekateko@uw.edu>
2018-07-02 10:57:41 -0700

    Make the menu close when a menu item is clicked

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -5,14 +5,47 @@ import Menu from 'react-burger-menu/lib/menus/slide'
 import './index.scss'
 
 class MobileMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuOpen: false,
+    }
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen })
+  }
+
+  // This is used to close the menu, e.g. when a user clicks a menu item
+  closeMenu() {
+    this.setState({ menuOpen: false })
+  }
+
   render() {
     return (
-      <Menu right width={'100%'}>
-        <Link to="/">HOME</Link>
-        <Link to="/harm">HARM</Link>
-        <Link to="/consequences">CONSEQUENCES</Link>
-        <Link to="/alternatives">ALTERNATIVES</Link>
-        <Link to="/contact">CONTACT</Link>
+      <Menu
+        right
+        width={'100%'}
+        isOpen={this.state.menuOpen}
+        onStateChange={state => this.handleStateChange(state)}
+      >
+        <Link to="/" onClick={() => this.closeMenu()}>
+          HOME
+        </Link>
+        <Link to="/harm" onClick={() => this.closeMenu()}>
+          HARM
+        </Link>
+        <Link to="/consequences" onClick={() => this.closeMenu()}>
+          CONSEQUENCES
+        </Link>
+        <Link to="/alternatives" onClick={() => this.closeMenu()}>
+          ALTERNATIVES
+        </Link>
+        <Link to="/contact" onClick={() => this.closeMenu()}>
+          CONTACT
+        </Link>
       </Menu>
     )
   }
