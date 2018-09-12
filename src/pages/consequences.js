51e4bc2e4344e7bb3774ea45ccc0d47c28177864
Jim Palowski <palowskijim@gmail.com>
2018-09-12 09:59:52 -0700

    Changed span tags to paragraph tags

--- src/pages/consequences.js
+++ src/pages/consequences.js
@@ -5,7 +5,6 @@ import Img from 'gatsby-image';
 import styles from './consequences.module.scss';
 import sharedStyles from '../assets/shared-styles/shared.module.scss';
 
-
 const ConsequencesPage = ({ data }) => (
   <div>
     <Img 
@@ -13,34 +12,34 @@ const ConsequencesPage = ({ data }) => (
       sizes={data.arrestedImage.sizes}
     />  
     <div className={sharedStyles.pageWrapper}>
-        <span className="riskBuying">Are you willing to risk the consequences of buying sex?</span>
+        <p className="riskBuying">Are you willing to risk the consequences of buying sex?</p>
         <div className={sharedStyles.dividerLine}></div>
-        <span className="offenderRegistry">You might end up on the Sex Offender Registry</span>
+        <p className="offenderRegistry">You might end up on the Sex Offender Registry</p>
         <Img
           alt="Text overlay that reads: I can't go to my kid's birthday because I'm a registered sex offender. Background image of a child blowing out some candles."
           sizes={data.childImage.sizes}  
         />
-        <span className="getArrested">Get arrested and you could spend up to 27 months in jail</span>
+        <p className="getArrested">Get arrested and you could spend up to 27 months in jail</p>
         <Img
           alt="Text overlay that reads: My whole world came crashing down, and I felt like I wanted to die. Background image of a man standing in a jail cell."  
           sizes={data.jailImage.sizes}
           />
-        <span className="payFines">You might pay up to $5,000 in fines, but you could lose much more than that</span>  
+        <p className="payFines">You might pay up to $5,000 in fines, but you could lose much more than that</p>  
           <Img
             alt="Text overlay that reads: I used to be my daughter's favorite, now she won't even look at me. Background image of a mother consoling her crying daughter."
             sizes={data.daughterImage.sizes}
           />
-        <span className="loseJob">You could become a liability to your company and lose your job</span>
+        <p className="loseJob">You could become a liability to your company and lose your job</p>
           <Img
             alt="Text overlay that reads: They said someone in h.r. saw my mugshot on the news. Background image of a man sitting at the bar."
             sizes={data.newsImage.sizes}
           />
-        <span className="rethinkBuying">Ready to rethink buying sex? There is help available for you now.</span>
+        <p className="rethinkBuying">Ready to rethink buying sex? There is help available for you now.</p>
           <a href="/" className="arrow-redirect"><Img 
             alt="Image of an ellipse circle with an arrow within it"
             resolutions={data.arrowImage.resolutions}
           /></a>
-        <span className="readLaws">read the laws here</span>
+        <p className="readLaws">read the laws here</p>
         </div>
     </div>  
 )
