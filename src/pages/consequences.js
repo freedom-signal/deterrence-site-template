a312b95e2f7941b78f7cbe0441c9e24f85b2b8de
Jim Palowski <palowskijim@gmail.com>
2018-09-13 14:36:09 -0700

    Refactored SCSS styling and consequences naming convetion for classNames

--- src/pages/consequences.js
+++ src/pages/consequences.js
@@ -12,34 +12,34 @@ const ConsequencesPage = ({ data }) => (
       sizes={data.arrestedImage.sizes}
     />  
     <div className={sharedStyles.pageWrapper}>
-        <h className="riskBuying">Are you willing to risk the consequences of buying sex?</h>
+        <h className={styles.riskBuying}>Are you willing to risk the consequences of buying sex?</h>
         <div className={sharedStyles.dividerLine}></div>
-        <p className="offenderRegistry">You might end up on the { data.site.siteMetadata.sex_offender_registry }</p>
+        <p className={styles.offenderRegistry}>You might end up on the { data.site.siteMetadata.sex_offender_registry }</p>
         <Img
           alt="Text overlay that reads: I can't go to my kid's birthday because I'm a registered sex offender. Background image of a child blowing out some candles."
           sizes={data.childImage.sizes}  
         />
-        <p className="getArrested">Get arrested and you could spend up to {data.site.siteMetadata.jail_time } in jail</p>
+        <p className={styles.getArrested}>Get arrested and you could spend up to {data.site.siteMetadata.jail_time } in jail</p>
         <Img
           alt="Text overlay that reads: My whole world came crashing down, and I felt like I wanted to die. Background image of a man standing in a jail cell."  
           sizes={data.jailImage.sizes}
           />
-        <p className="payFines">You might pay up to  { data.site.siteMetadata.fine } in fines, but you could lose much more than that</p>  
+        <p className={styles.payFines}>You might pay up to  { data.site.siteMetadata.fine } in fines, but you could lose much more than that</p>  
           <Img
             alt="Text overlay that reads: I used to be my daughter's favorite, now she won't even look at me. Background image of a mother consoling her crying daughter."
             sizes={data.daughterImage.sizes}
           />
-        <p className="loseJob">You could become a liability to your company and lose your job</p>
+        <p className={styles.loseJob}>You could become a liability to your company and lose your job</p>
           <Img
             alt="Text overlay that reads: They said someone in h.r. saw my mugshot on the news. Background image of a man sitting at the bar."
             sizes={data.newsImage.sizes}
           />
-        <p className="rethinkBuying">Ready to rethink buying sex? There is help available for you now.</p>
+        <p className={styles.rethinkBuying}>Ready to rethink buying sex? There is help available for you now.</p>
           <a href="/alternatives" className="arrow-redirect"><Img 
             alt="Image of an ellipse circle with an arrow within it"
             resolutions={data.arrowImage.resolutions}
           /></a>
-        <a><p className="readLaws">read the laws here</p></a>
+        <a><p className={styles.readLaws}>read the laws here</p></a>
         </div>
     </div>  
 )
