ee62f8208ecec50bb28ca23c15ee035f04ccf934
Jim Palowski <palowskijim@gmail.com>
2018-09-14 14:54:07 -0700

    Remove packages and added an extra space

--- src/pages/consequences.js
+++ src/pages/consequences.js
@@ -15,31 +15,31 @@ const ConsequencesPage = ({ data }) => (
         <h className={styles.riskBuying}>Are you willing to risk the consequences of buying sex?</h>
         <div className={sharedStyles.dividerLine}></div>
         <div className={styles.textWrapper}>
-        <p className={styles.offenderRegistry}>You might end up on the { data.site.siteMetadata.sex_offender_registry }</p>
-        <Img
-          alt="Text overlay that reads: I can't go to my kid's birthday because I'm a registered sex offender. Background image of a child blowing out some candles."
-          sizes={data.childImage.sizes}
-        />
-        <p className={styles.getArrested}>Get arrested and you could spend up to {data.site.siteMetadata.jail_time } in jail</p>
-        <Img
-          alt="Text overlay that reads: My whole world came crashing down, and I felt like I wanted to die. Background image of a man standing in a jail cell."  
-          sizes={data.jailImage.sizes}
-          />
-        <p className={styles.payFines}>You might pay up to  { data.site.siteMetadata.fine } in fines, but you could lose much more than that</p>  
+          <p className={styles.offenderRegistry}>You might end up on the { data.site.siteMetadata.sex_offender_registry }</p>
           <Img
-            alt="Text overlay that reads: I used to be my daughter's favorite, now she won't even look at me. Background image of a mother consoling her crying daughter."
-            sizes={data.daughterImage.sizes}
+            alt="Text overlay that reads: I can't go to my kid's birthday because I'm a registered sex offender. Background image of a child blowing out some candles."
+            sizes={data.childImage.sizes}
           />
-        <p className={styles.loseJob}>You could become a liability to your company and lose your job</p>
+          <p className={styles.getArrested}>Get arrested and you could spend up to {data.site.siteMetadata.jail_time } in jail</p>
           <Img
-            alt="Text overlay that reads: They said someone in h.r. saw my mugshot on the news. Background image of a man sitting at the bar."
-            sizes={data.newsImage.sizes}
-          />
-        <p className={styles.rethinkBuying}>Ready to rethink buying sex? There is help available for you now.</p>
-          <a href="/alternatives" className="arrow-redirect"><Img 
-            alt="Image of an ellipse circle with an arrow within it"
-            resolutions={data.arrowImage.resolutions}
-          /></a>
+            alt="Text overlay that reads: My whole world came crashing down, and I felt like I wanted to die. Background image of a man standing in a jail cell."  
+            sizes={data.jailImage.sizes}
+            />
+          <p className={styles.payFines}>You might pay up to  { data.site.siteMetadata.fine } in fines, but you could lose much more than that</p>  
+            <Img
+              alt="Text overlay that reads: I used to be my daughter's favorite, now she won't even look at me. Background image of a mother consoling her crying daughter."
+              sizes={data.daughterImage.sizes}
+            />
+          <p className={styles.loseJob}>You could become a liability to your company and lose your job</p>
+            <Img
+              alt="Text overlay that reads: They said someone in h.r. saw my mugshot on the news. Background image of a man sitting at the bar."
+              sizes={data.newsImage.sizes}
+            />
+          <p className={styles.rethinkBuying}>Ready to rethink buying sex? There is help available for you now.</p>
+            <a href="/alternatives" className="arrow-redirect"><Img 
+              alt="Image of an ellipse circle with an arrow within it"
+              resolutions={data.arrowImage.resolutions}
+            /></a>
         </div>  
         <a><p className={styles.readLaws}>read the laws here</p></a>
         </div> 
