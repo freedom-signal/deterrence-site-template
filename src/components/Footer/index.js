7397bd9d3d9338c4c55bc58ffece20b6ea77a735
Liz Rush <liz@seattleagainstslavery.org>
2019-01-17 14:53:33 -0800

    Add logo for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+
 import styles from './index.module.scss'
+
+import logo from '../../assets/images/NDHTTF-horizontal.png'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <span className={styles.copyright}>© 2019 Seattle Against Slavery</span>
+    <OutboundLink href="https://www.ndhttf.org/" target="_blank">
+      <img src={logo} alt="North Dakota Human Trafficking Taskforce logo" />
+    </OutboundLink>
   </footer>
 )
 
